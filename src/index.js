c6fa1464ccee509b0b46342186f8fcd546406937
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-07 09:58:46 -0800

    add standard and imperial temp units
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import './css/styles.css';
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
+    let unit = $("input:radio[name=temp]:checked").val();
+    // console.log(unit);
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -21,15 +23,17 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
-   function getElements(response) {
+    function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showClouds').text(`The cloudiness in ${city} is ${response.clouds.all}%`);
+      $('.showF').text(`The temperature in ${city} in ${unit} is ${response.main.temp}`);
+      $('.showWeather').text(`The weather in ${city} is ${response.weather[0].main}`);
+      $("#showWind").text(`The wind in ${city} is ${response.wind.deg}`);
+      $("#showSpeed").text(`The wind in ${city} is ${response.wind.speed}`);
     }
   });
 
   $('#joke').click(function() {
-    // const city = $('#location').val();
-    // $('#location').val("");
 
     let request = new XMLHttpRequest();
     const url = `https://api.chucknorris.io/jokes/random`;
@@ -44,7 +48,7 @@ $(document).ready(function() {
     request.open("GET", url, true);
     request.send();
 
-   function getElements(response) {
+    function getElements(response) {
       $('.showJoke').text(`Joke: ${response.value}`);
       
     }
